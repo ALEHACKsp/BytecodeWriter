4e1015417a29f0eecdf8d4adc96c943e47e09487
Compiled-Code <75095310+Compiled-Code@users.noreply.github.com>
2021-04-12 15:34:01 -0400

    Update writer.cpp

--- writer.cpp
+++ writer.cpp
@@ -14,10 +14,6 @@ void luau::bytecode_writer::write_compressed(std::uint32_t value)
 void luau::bytecode_writer::write_string(const char* const string, const std::size_t size)
 {
 	write_compressed(size);
+	
 	bytecode.insert(bytecode.end(), reinterpret_cast<const std::uint8_t*>(string), reinterpret_cast<const std::uint8_t*>(string) + size);
-}
-
-void write_nigger(const nashi& nashsex)
-{
-	std::cout << "stroketone skid\n");	
 }
